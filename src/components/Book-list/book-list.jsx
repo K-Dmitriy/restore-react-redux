10fbf989fed3ebb8877d0e55b120244581dca100
d.kozlov <d.kozlov@hottelecom.by>
2021-05-19 12:02:41 +0300

    receive data, send data to store

--- src/components/Book-list/book-list.jsx
+++ src/components/Book-list/book-list.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 
+import { withBookstoreService } from '../HOC';
 import BookListItem from '../Book-list-item';
 import './book-list.css';
 
-const BookList = ({ books = [] }) => {
+const BookList = (props) => {
+	const { books, bookstoreService } = props;
+
+	useEffect(() => {
+		const data = bookstoreService.getBooks();
+		console.log(data);
+
+		props.booksLoaded(data);
+	}, []);
+
 	return (
 		<ul>
 			{books.map((book) => {
@@ -18,4 +28,15 @@ const mapStateToProps = ({ books }) => {
 	return { books };
 };
 
-export default connect(mapStateToProps)(BookList);
+const mapDispatchToProps = (dispatch) => {
+	return {
+		booksLoaded: (newBooks) => {
+			dispatch({
+				type: 'BOOKS_LOADED',
+				payload: newBooks,
+			});
+		},
+	};
+};
+
+export default withBookstoreService()(connect(mapStateToProps, mapDispatchToProps)(BookList));
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,5 @@
 const initState = {
-	books: [
-		{ id: 1, title: 'Eloquent JavaScript', author: 'Marijn Haverbeke' },
-		{ id: 2, title: 'Clean Code', author: 'Robert C. Martin' },
-	],
+	books: [],
 };
 
 const reducer = (state = initState, action) => {
