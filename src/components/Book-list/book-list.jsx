f72d47ccddd01cbc5b9b5f46aa98cebb2146eeb7
d.kozlov <d.kozlov@hottelecom.by>
2021-05-19 16:46:48 +0300

    add compose for HOC

--- src/components/Book-list/book-list.jsx
+++ src/components/Book-list/book-list.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 
+import compose from '../../utils';
 import { withBookstoreService } from '../HOC';
 import { booksLoaded } from '../../actions';
 import BookListItem from '../Book-list-item';
@@ -24,4 +25,4 @@ const mapStateToProps = ({ books }) => ({ books });
 
 const mapDispatchToProps = { booksLoaded };
 
-export default withBookstoreService()(connect(mapStateToProps, mapDispatchToProps)(BookList));
+export default compose(withBookstoreService(), connect(mapStateToProps, mapDispatchToProps))(BookList);
